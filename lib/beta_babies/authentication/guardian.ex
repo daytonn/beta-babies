844e778a982248ba80e4a40ae27dcc0726224cbb
Dayton Nolan <me@daytonnolan.com>
2019-02-19 10:46:15 -0600

    Add Guardian implementation for Authenticating Accounts
    Following the guide
    [here](https://hexdocs.pm/guardian/tutorial-start.html#create-implementation-module)
    to add a Guardian implementation for the Authentication context.

---
+++ lib/beta_babies/authentication/guardian.ex
@@ -0,0 +1,14 @@
+defmodule BetaBabies.Authentication.Guardian do
+  use Guardian, otp_app: :beta_babies
+
+  alias BetaBabies.Authentication
+
+  def subject_for_token(account, _claims), do: {:ok, to_string(account.id)}
+
+  def resource_from_claims(%{"sub" => id}) do
+    case Authentication.get_account!(id) do
+      nil -> {:error, :resource_not_found}
+      acount -> {:ok, acount}
+    end
+  end
+end
