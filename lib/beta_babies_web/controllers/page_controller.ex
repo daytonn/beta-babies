e5fcd3c41f6779063150c110db49f4abbf8eba24
Dayton Nolan <me@daytonnolan.com>
2019-02-20 11:15:16 -0600

    Add protected page to demonstrate authenticated access
    This implements the Guardian [documentation step](https://hexdocs.pm/guardian/tutorial-start.html#controller)

--- lib/beta_babies_web/controllers/page_controller.ex
+++ lib/beta_babies_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule BetaBabiesWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def secret(conn, _) do
+    account = Guardian.Plug.current_resource(conn)
+    render(conn, "secret.html", current_account: account)
+  end
 end
